fe94e5943cc66ffe6bf45079bc9f714fd51358eb
Kundan Kumar <kundansonu@icloud.com>
2021-08-14 00:59:13 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { OneBancTransactionHistoryComponent } from './one-banc-transaction-history/one-banc-transaction-history.component';
 import { OneBancTransactionHistoryService } from './services/transaction-history.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, OneBancTransactionHistoryComponent ],
+  declarations: [ AppComponent, OneBancTransactionHistoryComponent ],
   providers: [
     OneBancTransactionHistoryService],
 
